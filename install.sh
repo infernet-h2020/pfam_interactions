27f3d52826210fbda2180e8cd12533c907f0b97f
Edoardo Sarti <5398739+EdoardoSarti@users.noreply.github.com>
2019-03-28 15:32:36 +0100

    ready for Andrea++

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ pip install -r python3_requirements.txt >/dev/null 2>&1
 # Download Pfam version and unzip files
 python3 ${DIR}/src/change_pfam_version.py ${version}
 
-echo "Decompressing." 
+echo "Decompressing.\n" 
 echo "Warning: the decompressed files will take up these amounts of disk space:"
 for fn in `find ${DIR}/db/external_resources/Pfam_${version}/database_files/ -name '*.gz'`
 do
@@ -46,7 +46,7 @@ done
 
 while true
 do
-read -r -p "Proceed with decompression? [Y/n] " input
+read -r -p "\nProceed with decompression? [Y/n] " input
 
 case $input in
     [yY][eE][sS]|[yY]|"")
