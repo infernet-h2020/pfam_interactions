68a294fbaebd5aa4b09c29c2af906ba8b608a661
Edoardo Sarti <5398739+EdoardoSarti@users.noreply.github.com>
2019-10-03 15:54:13 +0200

    pfam2pdb

--- src/pfam2pdb.py
+++ src/pfam2pdb.py
@@ -90,7 +90,7 @@ def main_parser():
 		if os.path.exists(options['output_path']):
 			if options['output_path'][-1] != '/':
 				options['output_path'] += '/'
-			options['results_folder'] = options['output_path'] + 'dca2pdb_results_' + datetime.datetime.now().strftime("%Y%m%d") + "/"
+			options['results_folder'] = options['output_path'] + 'pfam2pdb_results_' + datetime.datetime.now().strftime("%Y%m%d") + "/"
 			if not os.path.exists(options['results_folder']):
 				os.mkdir(options['results_folder'])
 		else:
